58dfb9e25c0893fd0c8d67f1ada1ded3bd797132
Zeu Capua <coding@zeu.dev>
2024-05-18 01:55:10 -0700

    setup db schema, pages, layouts

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,14 @@
-import { defineDb } from 'astro:db';
+import { defineDb, defineTable, column } from 'astro:db';
+
+const Drawing = defineTable({ columns: {
+  id: column.text({ primaryKey: true }),
+  canvasData: column.json(),
+  logo: column.text(),
+  artist: column.text(),
+  reports: column.number({ default: 0 })
+}});
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: { Drawing }
 });
